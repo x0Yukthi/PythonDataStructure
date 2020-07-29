efb4c5d5d726b7fa03b07a86a359c72fda335eb7
x0Yukthi <yukthi100399@gmail.com>
2020-07-29 16:53:52 +0530

    To find smallest in list

---
+++ List/smallestElement.py
@@ -0,0 +1,18 @@
+# Gives the smallest in list
+
+def smallest(list):
+    m = list[0]
+    for i in range(len(list)):
+        if list[i] < m:
+            m = list[i]
+    return m
+
+
+mylist = []
+l = int(input("Enter the length \n"))
+for i in range(0, l):
+    ele = int(input("enter the {0} element ".format(i)))
+    mylist.append(ele)
+
+print(mylist)
+print("Smallest in the list :", smallest(mylist))
