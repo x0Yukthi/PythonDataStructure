3eb30187f01c31531b77a3b2f2302a6b99183e24
x0Yukthi <yukthi100399@gmail.com>
2020-08-28 12:40:53 +0530

    Extract the multiple values of a particular key in nested dictionary

--- Dictionary/extractValues.py
+++ Dictionary/extractValues.py
@@ -1,15 +1,15 @@
-'''A program to extract the values of a perticular key from nested dictionary'''
+'''A program to extract the values of a particular key from nested dictionary'''
 
 D = {
     "D1": {"1": 4, "a": 67, "2": "dictionary0", "b": 864, "3": "Lists"},
     "D2": {"1": 547, "a": "hello", "2": 57895, "b": 2464, "3": "World"},
     "D3": {"1": "Python", "a": 54.234, "2": "Java", "b": "C++", "3": "Ruby"}
 }
-print("The nested Dictonary is:\n", str(D))
-ele = input("Enter the key hows values are to be extracted:\n")
-# noinspection PyUnboundLocalVariable
-newDict = dict(D)
+print("\nThe nested Dictionary is:\n", str(D))
+ele = input("\nEnter the key whos values are to be extracted:\n")
 
-r = [sub[ele] for sub in newDict.values() if ele in sub.keys()]
+# loop to find the values of key
+r = [sub[ele] for sub in D.values() if ele in sub.keys()]
 
-print("The values of {} are: {}".format(ele, r))
+# to print the extracted values of key
+print("\nThe values of {} are : {}".format(ele, r))
