23d29796b4831b58730508adda4a3b407b3490e3
x0Yukthi <yukthi100399@gmail.com>
2020-12-28 20:06:07 +0530

    Occurence of an element

---
+++ array/OccurenceCount.py
@@ -0,0 +1,13 @@
+#To get the number of occurrences of a specified element in an array
+
+import array as a
+
+ar = a.array('i',[34,67,84,67,80,32,54])
+print('Occurence of 67 -> ',ar.count(67))
+
+c,m=0,67
+for x in ar:
+    if x == m:
+        c=c+1
+
+print('Occurence -> ',c)
